7b309df2098d2e0c1401830a9b176ab269cc3b85
908760230 <908760230@qq.com>
2022-06-09 23:00:22 +0800

    platform config file

---
+++ core/Prerequisites.h
@@ -0,0 +1,12 @@
+#ifndef EXPLORE_PREREQUISITES_H
+#define EXPLORE_PREREQUISITES_H
+
+#include "Platform.h"
+
+namespace Explore {
+	class Plugin;
+	class PluginManager;
+}
+
+
+#endif // !EXPLORE_PREREQUISITES_H
